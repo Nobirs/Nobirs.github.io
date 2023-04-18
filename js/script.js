6bed3edf537b01efae0532784d98a265adffc9ec
Nobirs <Alex_Nobirs13@mail.ru>
2023-04-18 23:12:25 +0300

    Main functional of mobile version is added

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 let navImage = document.querySelector(".MenuImage");
 let navLinks = document.querySelectorAll(".navLink");
+let allDetails = document.querySelectorAll("details");
 
 for(let link of navLinks) {
     link.onclick = function(e) {
         navImage.click();
+        for (detail of allDetails) {
+            if (link.innerText != detail.querySelector('summary').innerText) {
+                detail.open = false;
+            }
+            else {
+                detail.open = true;
+            }
+        }
+        if (link.innerText == 'Contacts') {
+            let butterflyGif = document.querySelector(".butterfly_image");
+            butterflyGif.style.display = 'block';
+            setTimeout(() => {
+                butterflyGif.style.display = 'none';
+            }, 1800);
+        }
     };
 }
 
@@ -12,3 +28,4 @@ navImage.onclick = function(e) {
     navMenu.classList.toggle('active');
     document.querySelector("body").classList.toggle("stop_scroll");
 }
+
