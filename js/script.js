dcca10e1f455519ac9f5de74d1fda6c6cb6be87b
Nobirs <Alex_Nobirs13@mail.ru>
2023-04-20 07:28:51 +0300

    Fine working version. Set my projects, desktop version, write some personal information to About me...

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let navImage = document.querySelector(".MenuImage");
 let navLinks = document.querySelectorAll(".navLink");
 let allDetails = document.querySelectorAll("details");
+let footerContacts = document.querySelector('.ContactsFooter');
 
 for(let link of navLinks) {
     link.onclick = function(e) {
         navImage.click();
-        for (detail of allDetails) {
+        for (let detail of allDetails) {
             if (link.innerText != detail.querySelector('summary').innerText) {
                 detail.open = false;
             }
@@ -14,11 +15,14 @@ for(let link of navLinks) {
             }
         }
         if (link.innerText == 'Contacts') {
+            footerContacts.style.display = 'block';
             let butterflyGif = document.querySelector(".butterfly_image");
             butterflyGif.style.display = 'block';
             setTimeout(() => {
                 butterflyGif.style.display = 'none';
             }, 1800);
+        } else {
+            footerContacts.style.display = 'none';
         }
     };
 }
@@ -28,4 +32,43 @@ navImage.onclick = function(e) {
     navMenu.classList.toggle('active');
     document.querySelector("body").classList.toggle("stop_scroll");
 }
+
+
+let details = document.querySelectorAll("details");
+let mediaQueryList = window.matchMedia("(min-width: 600px)");
+
+
+function openDetails(e) {
+    if (e.matches) {
+        for(let detail of details) {
+            detail.setAttribute("open", "open");
+        }
+    } else {
+        for(let detail of details) {
+            detail.removeAttribute("open");
+        }
+    }
+}
+
+mediaQueryList.addListener(openDetails);
+
+
+// var forceOpen = window.getComputedStyle(details).getPropertyValue("--details-force-opened");
+
+// function forceState(isOpen) {
+//   if( isOpen ) {
+//     details.setAttribute("open", "open");
+//   } else {
+//     details.removeAttribute("open");
+//   }
+// }
+
+// if(forceOpen && "matchMedia" in window) {
+//   var mm1 = window.matchMedia(forceOpen);
+//   forceState(mm1.matches);
+//   mm1.addListener(function(e) {
+//     forceState(e.matches);
+//   });
+// }
+
 
